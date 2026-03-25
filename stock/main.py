6e53146e47b9b4d7041298d75fda84c6d87617e2
aryanyadavcoder <aryan9250yadav@gmail.com>
2026-03-25 15:48:21 +0530

    stock sell

--- stock/main.py
+++ stock/main.py
@@ -7,7 +7,6 @@ import json
 
 def downloadData(sharename):
     data = yf.download(sharename, start="2025-12-9", end="2026-03-10")
-
     data.to_csv(f"{sharename}.csv")
     return data
 
@@ -18,23 +17,17 @@ def readCSV(sharename):
 
 
 def readPrice(sharename):
-
     data = yf.download(sharename, period="1d", interval="1m")
-
-    price = data["Close"].iloc[-1].item()
-
+    price = round(data["Close"].iloc[-1].item(), 2)
     return price
 
 
 def onedaychart(sharename):
-
     data = yf.download(sharename, period="1d", interval="1m")
-
     return data
 
 
 def plotgraph(data, sharename):
-
     plt.plot(data["Close"])
     plt.title(sharename + " Chart")
     plt.xlabel("Time")
@@ -43,54 +36,45 @@ def plotgraph(data, sharename):
 
 
 def purchase(shares, date, quantity):
-
     price = readPrice(shares)
+    Total_Price = price*quantity
+    
     data = {
         "Share": shares,
         "Date": date,
         "Quantity": quantity,
-        "Avg_Price": price,
-        "current_price": price,
-        "Profit_loss": 0
-
+        "Total_Price":Total_Price,
+        "Avg_Price": round(price, 2),
+        "current_price": round(price, 2),
+        "Profit_loss": round(0, 2)
     }
-
     try:
         with open("purchase.json", "r") as f:
             purchase = json.load(f)
     except:
         purchase = []
-
     found = False
-
     for stock in purchase:
         if stock["Share"] == shares:
-
             old_qty = stock["Quantity"]
             old_price = stock["Avg_Price"]
-
             new_qty = old_qty + quantity
-
-            avg_price = ((old_price *old_qty) +(price* quantity))/new_qty
-
+            avg_price = ((old_price * old_qty) + (price * quantity))/new_qty
             profit_loss = (price-avg_price)*new_qty
-
+            Total_Price = avg_price*new_qty
             stock["Quantity"] = new_qty
-            stock["Avg_Price"] = round(avg_price, 2)
-            stock["current_price"] = round(price, 2)
-            stock["Profit_loss"] = round(profit_loss, 2)
+            stock["Avg_Price"] = avg_price
+            stock["current_price"] = price
+            stock["Profit_loss"] = profit_loss
+            stock["Total_Price"] = Total_Price
             found = True
             break
-
     if not found:
         purchase.append(data)
-
     with open("purchase.json", "w") as f:
         json.dump(purchase, f, indent=4)
-
     print("stock purchase save")
-    
-    
+
 
 def profit_loss_chart(share):
     with open("purchase.json", "r") as f:
@@ -104,20 +88,107 @@ def profit_loss_chart(share):
     else:
         print("Stock not found!")
         return
-
-    color = ["blue","orange"]
-    if profit_loss>0:
+    color = ["blue", "orange"]
+    if profit_loss > 0:
         color.append("green")
     else:
         color.append("red")
-            
-    x = ["Buy Price", "Current Price","Profit/loss"]
-    y = [buy_price, current_price,profit_loss]
-    
-    
-    plt.bar(x,y,color=color,label = share)
+    x = ["Buy Price", "Current Price", "Profit/loss"]
+    y = [buy_price, current_price, profit_loss]
+
+    plt.bar(x, y, color=color, label=share)
     plt.title(f"{share} Profit/Loss")
     plt.ylabel("Avg_Price")
     plt.xlabel("Current_Price")
     plt.legend()
+    plt.grid(axis='y', linestyle='--', alpha=0.5)
     plt.show()
+
+
+def sell(sharename, sell_quantity):
+    price = readPrice(sharename)
+    with open("purchase.json", "r") as f:
+        stocks = json.load(f)
+    for stock in stocks:
+        if stock["Share"] == sharename:
+            if sell_quantity > stock["Quantity"]:
+                print("Sell quantity not avalible")
+                return
+            buy_price = stock["Avg_Price"]
+            profit = (price - buy_price) * sell_quantity
+            stock["Quantity"] -= sell_quantity
+            remaining_qty = stock["Quantity"]
+            current_price = price
+            buy_price = stock["Avg_Price"]
+            profit_loss = (current_price-buy_price) * remaining_qty
+            stock["current_price"] = round(current_price, 2)
+            stock["Profit_loss"] = round(profit_loss, 2)
+            sell_data = {
+                "Share": sharename,
+                "Sell_Quantity": sell_quantity,
+                "Buy_Price": round(buy_price, 2),
+                "Sell_Price": round(price, 2),
+                "Profit/loss": round(profit, 2)
+            }
+            if stock["Quantity"] == 0:
+                stocks.remove(stock)
+            break
+    else:
+        print("Stock not found")
+        return
+    with open("purchase.json", "w") as f:
+        json.dump(stocks, f, indent=4)
+    try:
+        with open("sell.json", "r") as f:
+            sell_history = json.load(f)
+    except:
+        sell_history = []
+    found = False
+    for stock in sell_history:
+        if stock["Share"] == sharename:
+            stock["Sell_Quantity"] += sell_quantity
+            stock["Profit"] += round(profit, 2)
+            stock["Sell_Price"] = round(price, 2)
+            found = True
+            break
+    if not found:
+        sell_history.append(sell_data)
+    with open("sell.json", "w") as f:
+        json.dump(sell_history, f, indent=4)
+    print("Stock sold successfully")
+
+
+def profit_loss_details(sharename):
+    with open("purchase.json", "r") as f:
+        stocks = json.load(f)
+        price = readPrice(sharename)
+    for stock in stocks:
+        if stock["Share"] == sharename:
+
+            remaining_qty = stock["Quantity"]
+            current_price = price
+            buy_price = stock["Avg_Price"]
+            profit_loss = (current_price-buy_price) * remaining_qty
+            stock["current_price"] = round(current_price, 2)
+            stock["Profit_loss"] = round(profit_loss, 2)
+
+            print("Quantity :", stock["Quantity"])
+            print("stock_name :", stock["Share"])
+            print("Buy_Price :", stock["Avg_Price"])
+            print("Current_Price :", stock["current_price"])
+            print("Profit_loss :", stock["Profit_loss"])
+            return
+    print("Stock not found")
+
+
+def buy_quantity(sharename):
+    with open("purchase.json", "r") as f:
+        stocks = json.load(f)
+    for stock in stocks:
+        if stock["Share"] == sharename:
+            print("Avalible Quantity :", stock["Quantity"])
+            return stock["Quantity"]
+
+def view_chart(sharename,start,end):
+    data = yf.download(sharename,start=start,end=end) 
+    return data       
--- stock/menu.py
+++ stock/menu.py
@@ -10,7 +10,7 @@ with open("stock/stock.json", "r") as f:
 
 while True:
     print("<---menu--->")
-    print("0-Exit,1-Buy,2-Profit_loss_chart,3-View current price,4-chart")
+    print("0-Exit,1-Buy,2-sell,3-View current price and chart,4-Profit_loss_chart,5-show buy stock,6-view chart")
     option = int(input("Enter option\n"))
     if option == 0:
         print("Exit")
@@ -31,11 +31,17 @@ while True:
         break
 
     elif option == 2:
-        
-        sn.profit_loss_chart()
-        
-        
+        print("<---menu--->")
+        for key, value in shares.items():
+            print(key,value)
+        n = int(input("Enter stock number :"))
+        sharename = shares[str(n)]
+        sell_date = str(date.today())
+        sn.buy_quantity(sharename)
+        sell_quantity = int(input("Enter sell quantity :"))           
+        sn.sell(sharename, sell_quantity) 
         break
+        
 
     elif option == 3:
         print("<---Menu--->")
@@ -43,13 +49,10 @@ while True:
             print(key, value)
         n = int(input("Enter number\n"))
         sharename = shares[str(n)]
-
         prices = sn.readPrice(sharename)
         print("latest close price :", prices)
         data = sn.onedaychart(sharename)
-
         sn.plotgraph(data, sharename)
-
         break
 
     elif option == 4:
@@ -61,5 +64,26 @@ while True:
         stock = shares[str(n)]
         sn.profit_loss_chart(stock)
         break
+    
+    elif option == 5:
+        print("<---Menu--->")
+        for key, value in shares.items():
+            print(key, value)
+        n = int(input("Enter number\n"))
+        sharename = shares[str(n)]
+        sn.profit_loss_details(sharename)
+        break
+    
+    elif option == 6:
+        print("<---Menu--->")
+        for key, value in shares.items():
+            print(key, value)
+        n = int(input("Enter number\n"))
+        sharename = shares[str(n)]
+        start = input("Enter start date :")
+        end = input("Enter end date :")
+        data = sn.view_chart(sharename,start,end)
+        sn.plotgraph(data,sharename)
+    
     else:
         print("Invalid option")
